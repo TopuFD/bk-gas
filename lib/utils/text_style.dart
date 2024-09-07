2a68f91c646d67da0b75639d792f485299c21517
TopuFD <topuroy3317@gmail.com>
2024-09-07 22:48:08 +0600

    add account profile screen

---
+++ lib/utils/text_style.dart
@@ -0,0 +1,29 @@
+
+import 'package:bk_gas/utils/color.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class CustomTextStyle {
+  //==============================================heading one
+  static TextStyle h1({Color? color,FontWeight? fontWeight,double? fontSize,double? letterSpacing,
+  }) {
+    return TextStyle(
+      color: color ?? AppColor.primaryColor,
+      fontSize: fontSize ?? 25.sp,
+      letterSpacing: letterSpacing,
+      fontWeight: fontWeight ?? FontWeight.bold,
+    );
+  }
+  //==============================================heading two
+  static TextStyle h2({Color? color,FontWeight? fontWeight,double? fontSize,double? letterSpacing,
+  }) {
+    return TextStyle(
+      color: color ?? AppColor.textblack,
+      fontSize: fontSize ?? 22.sp,
+      letterSpacing: letterSpacing,
+      fontWeight: fontWeight ?? FontWeight.bold,
+    );
+  }
+
+
+}
--- lib/view/account_screen/account_profile_screen.dart
+++ lib/view/account_screen/account_profile_screen.dart
@@ -2,8 +2,9 @@ import 'dart:io';
 
 import 'package:bk_gas/controller/image_controller.dart';
 import 'package:bk_gas/utils/color.dart';
+import 'package:bk_gas/utils/text_style.dart';
 import 'package:bk_gas/widget/custom_appber.dart';
-import 'package:bk_gas/widget/heading_text.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
@@ -12,63 +13,113 @@ import 'package:get/get.dart';
 class AccountProfileScreen extends StatelessWidget {
   AccountProfileScreen({super.key});
 
-  
   ImagePicController imageController = ImagePicController();
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const CustomAppbar(title: "Account",),
+      appBar: const CustomAppbar(
+        title: "Account",
+      ),
       body: SizedBox(
         width: Get.width,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(height: 30.h,),
-            Stack(
-              clipBehavior: Clip.none,
-              children: [
-                Container(
-                    height: 130,
-                    width: 130,
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: AppColor.textlightBlue,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 15.w),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 30.h,
+              ),
+              Stack(
+                clipBehavior: Clip.none,
+                children: [
+                  Container(
+                      height: 130,
+                      width: 130,
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: AppColor.textlightBlue,
+                        ),
+                        shape: BoxShape.circle,
                       ),
-                      shape: BoxShape.circle,
-                    ),
-                    child: ClipOval(
-                      child: Obx(() {
-                        return imageController.imagePath!.isNotEmpty
-                            ? Image.file(
-                                File(imageController.imagePath!.value),
-                                fit: BoxFit.cover,
-                                height: 130.h,
-                                width: 130.w,
-                              )
-                            : Icon(
-                                Icons.person,
-                                color: AppColor.secondaryColor,
-                                size: 100.h,
-                              );
-                      }),
-                    )),
-                Positioned(
-                    bottom: -14,
-                    right: -22,
-                    child: IconButton(
-                        onPressed: () {
-                          showModalSheet(context);
-                        },
-                        icon: Icon(
-                          Icons.image_outlined,
-                          color: AppColor.primaryColor,
-                          size: 42.h,
-                        )))
-              ],
-            ),
-            const HeadingText(headingText: "")
-          ],
+                      //=================================profile image here=================>
+                      child: ClipOval(
+                        child: Obx(() {
+                          return imageController.imagePath!.isNotEmpty
+                              ? Image.file(
+                                  File(imageController.imagePath!.value),
+                                  fit: BoxFit.cover,
+                                  height: 130.h,
+                                  width: 130.w,
+                                )
+                              : Icon(
+                                  Icons.person,
+                                  color: AppColor.secondaryColor,
+                                  size: 100.h,
+                                );
+                        }),
+                      )),
+                  Positioned(
+                      bottom: -14,
+                      right: -22,
+                      child: IconButton(
+                          onPressed: () {
+                            showModalSheet(context);
+                          },
+                          icon: Icon(
+                            Icons.image_outlined,
+                            color: AppColor.primaryColor,
+                            size: 42.h,
+                          )))
+                ],
+              ),
+              SizedBox(
+                height: 10.h,
+              ),
+              Text(
+                "Subrina Carpernter",
+                style: CustomTextStyle.h2(),
+              ),
+              TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Edit Account",
+                    style: CustomTextStyle.h1(fontSize: 16),
+                  )),
+              userCard(iconData: Icons.person, title: "Sabrina Carpenter"),
+              userCard(iconData: Icons.email, title: "sabrina@ymail.com"),
+              userCard(
+                  heights: 100.h,
+                  iconData: CupertinoIcons.location_solid,
+                  title: "2972 Westheimer Rd. Santa Ana,Illinois 85486"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  //===========================userDataCard=====================
+  userCard({String? title, double? heights, IconData? iconData}) {
+    return Container(
+      height: heights ?? 60.h,
+      width: Get.width,
+      margin: EdgeInsets.only(bottom: 10.h),
+      decoration: BoxDecoration(
+        border: Border.all(color: AppColor.primaryColor),
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: Center(
+        child: ListTile(
+          leading: Icon(
+            iconData,
+            color: AppColor.primaryColor,
+          ),
+          title: Text(
+            title.toString(),
+            style: CustomTextStyle.h2(fontSize: 14, color: AppColor.textgrey),
+          ),
         ),
       ),
     );
